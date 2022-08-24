3189261021e3bc785c982420705907d18a9042c7
cwitmer34 <connorwitmer34@gmail.com>
2022-08-24 00:07:05 -0400

    Added 404 Page Route

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT;
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
 
 app.listen(PORT, console.log(`App is listening on ${PORT}`));
+
+console.log("balls");
