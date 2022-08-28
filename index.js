b239e6b1db8d28ced8130d2a623d47a57a2d8d3b
cwitmer34 <connorwitmer34@gmail.com>
2022-08-28 10:10:06 -0400

    Custom stylesheet added

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
+// Modules & Globals
 require("dotenv").config();
-
 const PORT = process.env.PORT;
-
 const express = require("express");
 const app = express();
 
+// Express Settings
+app.set("views", __dirname + "/views");
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
 
+// Controllers & Routes
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
-
 
 app.get("*", (req, res) => {
   res.status(404).render("error404");
