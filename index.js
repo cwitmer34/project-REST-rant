a6916ff99750b7ecfdb24c3e73f46d396296b91b
cwitmer34 <connorwitmer34@gmail.com>
2022-08-28 07:37:02 -0400

    Added 404 page view

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.status(404).send("<h1>404 Page</h1>");
+  res.status(404).render("error404");
 });
 
 app.listen(PORT, console.log(`App is listening on http://localhost:${PORT}`));
