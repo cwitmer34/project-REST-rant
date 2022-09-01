3d3482a43e912e34125c3057188c7ea5c28b677c
cwitmer34 <connorwitmer34@gmail.com>
2022-09-01 10:40:49 -0400

    form page styled

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -6,6 +6,29 @@ function new_form() {
     <Def>
       <main>
         <h1>New Location</h1>
+        <form action="/places" method='POST'>
+          <div className='form-group'>
+            <label htmlFor="name">Location Name</label>
+            <input className='form-control' type="text" name='name' id='name' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="image">Image of Location</label>
+            <input className='form-control' type="url" name='image' id='image'/>
+          </div>
+          <div className='form-group'>
+            <label htmlFor="cuisines">Cuisines</label>
+            <input className='form-control' type="text" name='cuisines' id='cuisines' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="city">City of Location</label>
+            <input className='form-control' type="text" name='city' id='city' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor="state">State of Location</label>
+            <input className='form-control' type="text" name='state' id='state' />
+          </div>
+          <input className='btn btn-primary' type="submit" value={"Add Place"}/>
+        </form>
       </main>
     </Def>
   )
